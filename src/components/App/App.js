74da3e04329e694a4184a0fb28f12dc5f58cba6d
Joshua Nicholas Pearson <jnpearson4850@gmail.com>
2018-08-27 16:28:30 -0400

    11 of 19

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Header from '../Header/Header'
+import ContactList from '../ContactList/ContactList'
+import NewContact from '../NewContact/NewContact'
+import { Route, Switch } from 'react-router-dom'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contacts: this.props.contacts
+    }
+  }
   render() {
-    return (
-      <div className="">
-        <p>app</p>
-      </div>
-    );
+    return <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path='/' component={ContactList} />
+        <Route path='/new-contact' component={NewContact} />
+      </Switch>
+    </div>
   }
 }
 
---
+++ src/components/NewContact/NewContact.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NewContact extends Component {
+    render() {
+        return (
+            <div className="contact">
+                <p>app</p>
+            </div>
+        );
+    }
+}
+
+export default NewContact;
