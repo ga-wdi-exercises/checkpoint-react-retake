f40ac85e64bdca6212dc8367f000c701bb0e9d59
Lovdeep <lovdeepgahunia1@gmail.com>
2018-12-10 10:21:45 -0500

    checkpoint retake

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import { Route } from "react-router";
+import ContactList from "../ContactList/ContactList";
+import NewContact from "../NewContact/NewContact";
+
+import Header from "../Header/Header";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: this.props.contacts
+    };
+  }
   render() {
     return (
-      <div className="">
-        <p>app</p>
+      <div className="App">
+        <Header />
+        <Route
+          path="/"
+          render={() => <ContactList contacts={this.state.contacts} />}
+        />
+        <Route path="/new-contact" component={NewContact} />
       </div>
     );
   }
---
+++ src/components/NewContact/NewContact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class NewContact extends Component {
+  render() {
+    return (
+      <div>
+        <h1> New Contact </h1>
+        <form>
+          <input name="name" type="text-box" />
+          <input name="email" type="text-box" />
+          <input name="profile_picture" type="text-box" />
+        </form>
+      </div>
+    );
+  }
+}
+export default NewContact;
