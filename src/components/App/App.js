f436512122b3a3153712f968aa2768d2bbbf147b
“Christina <cfthompson321@gmail.com>
2018-12-10 10:02:02 -0500

    react-retake

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      constacts: this.props.contacts
+    };
+    this.setContacts = this.setContacts.bind(this);
+  }
   render() {
     return (
-      <div className="">
+      <div className="App">
         <p>app</p>
+        <main>
+          <Route path="/" exact component={Header} />
+          <Route path="/" exact component={ContactList} />
+        </main>
       </div>
     );
   }
