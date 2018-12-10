37c2100851db848ebb68e0ebfe7754ae0936e0e6
MJ Sandoval <salssedan@gmail.com>
2018-12-10 09:59:41 -0500

    salman naseer react-retake

---
+++ src/components/NewContact/NewContact.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class NewContact extends Component {
+    render() {
+        return (
+        <div>
+            <h1>New Contact</h1>
+            <form>
+                <input name='name' type='text-box' />
+                <input name='email' type='text-box' />
+                <input name='profile_picture' type='text-box' />
+                <input name='super_power' type='text-box' />
+            </form>
+        </div>
+        )
+    }
+}
+
+export default NewContact
