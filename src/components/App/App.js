30bb94b3f714b271e2c1918a32779f65e8564065
Roger Campbell II <rc1336@gmail.com>
2019-02-19 14:36:32 -0500

    Yea..

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import contacts from "../../contacts.json";
+import Header from "../Header/Header";
+import ContactList from "../ContactList/ContactList";
 
 class App extends Component {
   render() {
     return (
-      <div className="">
+      <div className="App">
+        <Header />
+        <Route path="/">
+          <ContactList />
+        </Route>
         <p>app</p>
       </div>
     );
